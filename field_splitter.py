2f325ef9b3ff408815e36d9ddf0e8cafbcf02ece
lglanville <lachlanglanville@gmail.com>
2020-01-16 16:17:32 +1100

    modified:   field_splitter.py added delimiter option, now uses argparse

--- field_splitter.py
+++ field_splitter.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import csv
 
 
@@ -8,9 +8,14 @@ def get_reader(path):
     return(reader)
 
 
-def split_fields(row, fieldname, heads):
+def split_fields(row, fieldname, heads, delim=None):
     if row.get(fieldname) is not None:
         subjects = row.pop(fieldname).strip().splitlines()
+        if delim is not None:
+            for sub in subjects:
+                new_subs = [s.strip('.') for s in sub.split(delim)]
+                subjects.remove(sub)
+                subjects.extend(new_subs)
         sub_heads = [f'{fieldname}({x})' for x in range(1, len(subjects)+1)]
         for head in sub_heads:
             if head not in heads:
@@ -20,20 +25,33 @@ def split_fields(row, fieldname, heads):
     return row, heads
 
 
-reader = get_reader(sys.argv[1])
-data = {'headings': reader.fieldnames, 'rows': []}
-fields = [
-    'EADSubject_tab', 'EADName_tab', 'EADGeographicName_tab',
-    'EADLanguageOfMaterial_tab', 'EADLanguageCodeOfMaterial_tab']
-for row in reader:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Split multi-value fields in Emu uplaod sheets')
+    parser.add_argument('input', metavar='i', help='csv upload file')
+    parser.add_argument('output', metavar='o', help='converted csv file')
+    parser.add_argument(
+        '--delimiter', '-d',
+        help='delimiter for values. Script will always split on new lines,'
+        'but if a character is specified here it will '
+        'also split on that character')
+
+    args = parser.parse_args()
+    reader = get_reader(args.input)
+    data = {'headings': reader.fieldnames, 'rows': []}
+    fields = [
+        'EADSubject_tab', 'EADName_tab', 'EADGeographicName_tab',
+        'EADLanguageOfMaterial_tab', 'EADLanguageCodeOfMaterial_tab']
+    for row in reader:
+        for field in fields:
+            row, heads = split_fields(
+                row, field, data['headings'], delim=args.delimiter)
+            data['headings'] = heads
+        data['rows'].append(row)
     for field in fields:
-        row, heads = split_fields(row, field, data['headings'])
-        data['headings'] = heads
-    data['rows'].append(row)
-for field in fields:
-    if field in data['headings']:
-        data['headings'].remove(field)
-with open(sys.argv[2], 'w', newline='', encoding='utf-8') as f:
-    writer = csv.DictWriter(f, data['headings'])
-    writer.writeheader()
-    writer.writerows(data['rows'])
+        if field in data['headings']:
+            data['headings'].remove(field)
+    with open(args.output, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, data['headings'])
+        writer.writeheader()
+        writer.writerows(data['rows'])
