9dfcad2204b411bcfe7bce0683482a268d3f3a80
lglanville <lachlanglanville@gmail.com>
2020-12-10 13:29:51 +1100

    Now supports multiple redactions, timestamp formats, additional args for ffmpeg

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Emu-scripts
-## A bunch of scripts to make uploads into EMu easier.
+# UMA scripts
+## A bunch of scripts for common University of Melbourne Archives tasks and issues.
 
 ### date_extractor.py
 Pulls all dates out of a column in an Excel workbook and reformats them to
@@ -20,3 +20,7 @@ into separate columns so that EMu can ingest them correctly.
 ### upload_parse.py
 Inspect a delimited upload sheet and optional location sheet for obvious
 problems such as missing mandatory fields, incorrect date formats etc.
+
+### av_redacter.py
+Simple wrapper script for ffmpeg that cuts a segment out of an input AV file
+to create a redacted version.
--- av_redacter.py
+++ av_redacter.py
@@ -1,20 +1,69 @@
 import argparse
 from pathlib import Path
 import subprocess
+from datetime import timedelta
 
 
-def redact_complex(file, beginning, end):
+def conv_to_seconds(tstring):
+    if tstring.isdigit():
+        return(int(tstring))
+    else:
+        h, m, s = tstring.split(':')
+        t = timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+        return t.seconds
+
+
+def get_segments(redactions):
+    redactions.sort()
+    segments = []
+    start = 0
+    for rstart, rend in redactions:
+        segment = (start, rstart)
+        if segment != (0, 0):
+            segments.append(segment)
+        start = rend
+    segments.append((start, None))
+    return segments
+
+
+def get_filter_args(segments):
+    args = ''
+    seg_num = 0
+    for start, end in segments:
+        seg_num += 1
+        if end is not None:
+            args += f'[0:v]trim=start={start}:end={end},setpts=PTS-STARTPTS,format=yuv420p[{seg_num}v];'
+            args += f'[0:a]atrim=start={start}:end={end},asetpts=PTS-STARTPTS[{seg_num}a];'
+        else:
+            args += f'[0:v]trim=start={start},setpts=PTS-STARTPTS,format=yuv420p[{seg_num}v];'
+            args += f'[0:a]atrim=start={start},asetpts=PTS-STARTPTS[{seg_num}a];'
+    for x in range(1, seg_num+1):
+        args += f'[{x}v][{x}a]'
+    args += f'concat=n={seg_num}:v=1:a=1[outv][outa]'
+    return args
+
+
+def redact(file, redactions, input_args=None, output_args=None):
     file = Path(file)
     outfile = Path(file.parent, file.stem + '.REDACTED' + file.suffix)
     if outfile.exists():
         outfile.unlink()
-    sp = subprocess.run([
+    segments = get_segments(redactions)
+    filter_args = get_filter_args(segments)
+    args = [
         'ffmpeg', '-i', str(file), '-filter_complex',
-        f'[0:v]trim=duration={beginning}[av];[0:a]atrim=duration={beginning}[aa];'
-        f'[0:v]trim=start={end},setpts=PTS-STARTPTS[bv];'
-        f'[0:a]atrim=start={end},asetpts=PTS-STARTPTS[ba];'
-        '[av][bv]concat[outv];[aa][ba]concat=v=0:a=1[outa]',
-        '-map', '[outv]', '-map', '[outa]', str(outfile)], stdout=subprocess.PIPE)
+        filter_args,
+        '-map', '[outv]', '-map', '[outa]', str(outfile)]
+    if input_args is not None:
+        for arg in input_args:
+            i = args.index('-i')
+            args.insert(i, arg)
+    if output_args is not None:
+        for arg in input_args:
+            i = args.index(str(outfile))
+            args.insert(i, arg)
+    sp = subprocess.run(args, stdout=subprocess.PIPE)
+    print(sp.args)
 
 
 if __name__ == '__main__':
@@ -23,11 +72,15 @@ if __name__ == '__main__':
         'file using ffmpeg. Requires the ffmpeg executable to be on PATH')
     parser.add_argument('input', metavar='i', help='input av file')
     parser.add_argument(
-        '--start', '-s',
-        help='start of redaction (in seconds or HH:MM:SS.xxx format)')
+        '--redactions', '-r', nargs='+',
+        help='start and end of redactions (in seconds or HH:MM:SS format)')
     parser.add_argument(
-        '--end', '-e',
-        help='end of redaction (in seconds or HH:MM:SS.xxx format)')
+        '--inputargs', '-ia', nargs='+',
+        help='additional input parameters for ffmpeg')
+    parser.add_argument(
+        '--outputargs', '-oa', nargs='+',
+        help='additional output parameters for ffmpeg')
 
     args = parser.parse_args()
-    redact_complex(args.input, args.start, args.end)
+    redactions = [[conv_to_seconds(i) for i in r.split('-')] for r in args.redactions]
+    redact(args.input, redactions, input_args=args.inputargs, output_args=args.outputargs)
